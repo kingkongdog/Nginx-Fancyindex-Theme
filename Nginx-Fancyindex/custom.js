63ad8564eb8adcbfcb7786a85d9aa45ebf3e041a
ligang <kingkongdog9527@gmail.com>
2026-08-29 00:38:21 +0800

    feat: 设置 title

--- Nginx-Fancyindex/custom.js
+++ Nginx-Fancyindex/custom.js
@@ -2,8 +2,8 @@ $(() => {
   $('.breadcrumb li a').first().remove();
   
   const segments = location.pathname.split('/').filter(Boolean)
+  document.title = segments[0]
   if(segments.length === 1) {
     $('#list tbody tr').first().remove();
-    document.title = segments[0]
   }
 })
