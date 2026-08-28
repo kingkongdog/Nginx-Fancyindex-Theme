9e14cd14af914a255abf5bc2d90dcb23de828b21
ligang <kingkongdog9527@gmail.com>
2026-08-29 00:08:39 +0800

    feat: custom

--- Nginx-Fancyindex/custom.js
+++ Nginx-Fancyindex/custom.js
@@ -1,3 +1,7 @@
 $(() => {
-  $('.breadcrumb li a').href = ''
+  $('.breadcrumb li a').remove();
+  
+  if(location.pathname.split('/').filter(Boolean).length === 1) {
+    $('#list tbody tr').first().remove();
+  }
 })
